eb1bcbe1cbbe6149a3c49c3bc30f5dfd8976d6f9
david23reg83-cmyk <david23reg83@gmail.com>
2026-02-13 15:37:37 +0300

    Первая программа на Go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Devops!")
+	fmt.Println("Go version: 1.26")
+
+	fmt.Println("Я учу Go для DevOps!")
+	fmt.Println("2+2=", 2+2)
+	fmt.Println("10*5=", 10*5)
+	fmt.Println("100/4=", 100/4)
+	fmt.Println("250/4", 250/4)
+	fmt.Println("23*3=", 23*3)
+	fmt.Println("20-7=", 20-7)
+}
