be64aa58b3efc42a896818fe917eb0752eaac885
david23reg83-cmyk <david23reg83@gmail.com>
2026-02-16 16:20:22 +0300

    Моя первая программа

---
+++ practice.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// твой возраст
+	age := 26
+
+	//твое имя
+	name := "David"
+
+	// Сколько часов в день планируешь учить Go
+	hoursPerDay := 3
+
+	//Вывод информации
+	fmt.Println("Имя:", name)
+	fmt.Println("Возраст", age)
+	fmt.Println("учу Go часов в день:", hoursPerDay)
+}
