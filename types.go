07650193bf3f707552ab68ba2f4612567eb83c47
david23reg83-cmyk <david23reg83@gmail.com>
2026-02-17 20:31:02 +0300

    Добавить types.go

---
+++ types.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Целое число (int)
+	возраст := 25
+
+	// Текст (string)
+	имя := "Давид"
+
+	// Дробное число (float)
+	рост := 1.75
+
+	// Да/Нет (bool)
+	learning := true
+
+	fmt.Println("Имя", имя)
+	fmt.Println("Возраст", возраст)
+	fmt.Println("Рост", рост)
+	fmt.Println("Учу Go", learning)
+}
